7ad266e6daf1754d1fe122c7f470c163c2e155c0
ngirimana <chadrackngirimana@gmail.com>
2020-06-14 10:09:31 +0200

    Updates

---
+++ precache-manifest.639ca2faae9de075a07243cfa634aeeb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc89398c1917d88bfb68195e387d3e5",
+    "url": "/murika-front-end/index.html"
+  },
+  {
+    "revision": "877b9666e88f8ac83ecb",
+    "url": "/murika-front-end/static/css/3.a542658b.chunk.css"
+  },
+  {
+    "revision": "b04ec67999970cdd6168",
+    "url": "/murika-front-end/static/css/4.a542658b.chunk.css"
+  },
+  {
+    "revision": "44f36bd32f9163f084c7",
+    "url": "/murika-front-end/static/css/main.e41fa7bf.chunk.css"
+  },
+  {
+    "revision": "a6c2db687ba2eb2c8cad",
+    "url": "/murika-front-end/static/js/2.16e74cd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/murika-front-end/static/js/2.16e74cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877b9666e88f8ac83ecb",
+    "url": "/murika-front-end/static/js/3.dd0d5b0a.chunk.js"
+  },
+  {
+    "revision": "b04ec67999970cdd6168",
+    "url": "/murika-front-end/static/js/4.b146e768.chunk.js"
+  },
+  {
+    "revision": "44f36bd32f9163f084c7",
+    "url": "/murika-front-end/static/js/main.172d8669.chunk.js"
+  },
+  {
+    "revision": "95d0b1add9b8b4b47927",
+    "url": "/murika-front-end/static/js/runtime-main.6a285523.js"
+  },
+  {
+    "revision": "5120ad222b9080b343187d3c51e851f3",
+    "url": "/murika-front-end/static/media/house-1.5120ad22.jpeg"
+  }
+]);
