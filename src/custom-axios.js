0b900c97f0dd72f68ccb1a359bd400945399f747
ngirimana <chadrackngirimana@gmail.com>
2020-07-06 12:43:08 +0200

    remove space

--- src/custom-axios.js
+++ src/custom-axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:4000/api/v1"
-    : "http://murika.herokuapp.com/api/v1";
+    ? "http://localhost:4000/api/v1" : "http://murika.herokuapp.com/api/v1";
     console.log(token)
 const instance = axios.create({
   baseURL: baseURL,
