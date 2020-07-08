952b59f2a2752f0bd9c85eb60503c0842f9ceb0e
ngirimana <chadrackngirimana@gmail.com>
2020-07-08 20:31:40 +0200

    - add house description - add responsiveness to mobile device
    -[Continued #173666791]

--- src/containers/singleHouse/SingleHouse.jsx
+++ src/containers/singleHouse/SingleHouse.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import SlideShoww from "../../components/UI/SlideShoww/SlideShoww";
 import classes from "./singleHouse.module.scss";
-
+import PhotoIcon from "../../assets/images/iconfinder_33_111001.svg";
+import Button from "../../components/UI/Button/Button";
 
 class SingleHouse extends Component {
   render() {
@@ -10,8 +11,89 @@ class SingleHouse extends Component {
         {" "}
         <div className={classes.header}>
           <div className={classes.SlideShow}>
-            <div><SlideShoww /></div>
+            <SlideShoww />
+            <div className={classes.SlideBtn}>
+              <button className={classes.HouseButton}>
+                {" "}
+                <span>
+                  {" "}
+                  <img src={PhotoIcon} className={classes.PhotoIcon} alt="" />
+                </span>{" "}
+                <span className={classes.BtnText}> Photos</span>
+              </button>
+              <button className={classes.HouseButton}>
+                <span>
+                  {" "}
+                  <img src={PhotoIcon} className={classes.PhotoIcon} alt="" />
+                </span>{" "}
+                <span className={classes.BtnText}> video</span>
+              </button>
+            </div>
           </div>
+          <div className={classes.HouseDetails}>
+            <h3 className={classes.HouseTitle}>House Details</h3>
+            <div className={classes.HouseDesc}>
+              <span className={classes.Details}>
+                <label htmlFor="">House Type</label> <label>: Duplex</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Price For Rent</label>{" "}
+                <label>: 500000 RWF/Month</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Price Status</label>{" "}
+                <label>: Negotiable</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Minimum Rent </label>{" "}
+                <label>: 2 Months</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Number Of Rooms </label> <label>: 5</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Number Of bedrooms </label> <label>: 3</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Number Of bethrooms </label>{" "}
+                <label>: 2</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Location </label>{" "}
+                <label>: Kigali,Nyarugenge, Gitega,Gitega</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Size </label> <label>: 20* 25</label>
+              </span>
+              <span className={classes.Details}>
+                <label htmlFor="">Date Listed </label>{" "}
+                <label>: 20/06/2020</label>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={classes.otherDesc}>
+          <h3 className={classes.HouseTitle}>House Description</h3>
+          <p className={classes.OtherDetails}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+            earum dicta hic commodi soluta accusantium magnam voluptates iste id
+            maiores placeat, culpa veniam nulla fugiat unde aliquid quos nobis
+            praesentium voluptate dolore ut sunt corporis! Sint, aspernatur.
+            Corrupti, eos consequatur?
+          </p>
+        </div>
+        <div className={classes.otherDesc}>
+          <h3 className={classes.HouseTitle}>OtherDescription</h3>
+          <p className={classes.OtherDetails}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+            earum dicta hic commodi soluta accusantium magnam voluptates iste id
+            maiores placeat, culpa veniam nulla fugiat unde aliquid quos nobis
+            praesentium voluptate dolore ut sunt corporis! Sint, aspernatur.
+            Corrupti, eos consequatur?
+          </p>
+        </div>
+        <div className={classes.FullInfos}>
+          <Button btnType="Success">Get Full Information</Button>
         </div>
       </div>
     );
---
+++ src/store/reducers/SingleHouse/SingleHouse.js
@@ -0,0 +1,40 @@
+import * as actionTypes from "../../actions/actionTypes";
+import { updateObject } from "../../../shared/utility";
+
+const initialState = {
+  houseData: [],
+  loading: false,
+  error: "",
+};
+
+const fetchSingleHouseStart = (state, action) => {
+  return updateObject(state, { loading: true });
+};
+
+const fetchSingleHouseSuccess = (state, action) => {
+  return updateObject(state, {
+    houseData: action.houseData,
+    loading: false,
+  });
+};
+const fetchSingleHouseFail = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    error: action.error,
+  });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_SINGLE_HOUSE_START:
+      return fetchSingleHouseStart(state, action);
+    case actionTypes.FETCH_SINGLE_HOUSE_SUCCESS:
+      return fetchSingleHouseSuccess(state, action);
+    case actionTypes.FETCH_SINGLE_HOUSE_FAIL:
+      return fetchSingleHouseFail(state, action);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
